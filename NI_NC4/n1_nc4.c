a5f78c7ec910c88e11b07a3142c853ff53952a63
MoniqueMSouza <moniquemoreira.mkt@gmail.com>
2025-10-02 13:00:30 -0300

    Primeiro commit

---
+++ NI_NC4/n1_nc4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+    int idade = 25; 
+    int Idade;
+    int IDADE;
+    int idadE;
+    int quantidade = 2;
+    float altura = 1.75;
+    char letra = 'A'; //Entre aspas simples ''
+    char nome[20] = "Monique";
+    int _teste;
+    // int 2teste - > NÃO PODE INICIAR VARIÁVEL COM NÚMERO
+
+    idadE = 30; 
+
+
+
+
+
+}
