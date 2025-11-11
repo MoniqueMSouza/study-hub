bcd61915e2ee3a918b4cf65539f242620f601498
MoniqueMSouza <moniquemoreira.mkt@gmail.com>
2025-11-11 06:36:51 -0300

    feat: programa em C para demonstrar modificadores de tipo inteiro

---
+++ N3_NC1/NC_NC2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    unsigned long int numeroGrandePositivo = 4000000000;
+    unsigned int numeroPositivo = 4000000000;
+    long int numeroGrande = 4000000000;
+    long long int numeroGrande2x= 4000000000;
+    int numero = 4000000000;
+
+    printf("Número Grande Positivo: %lu\n", numeroGrandePositivo);
+    printf("Número Positivo: %u\n", numeroPositivo);
+    printf("Número Grande: %ld\n", numeroGrande);
+    printf("Número Grande 2x: %lld\n", numeroGrande2x);
+    printf("Número: %d\n", numero);
+    
+    return 0;
+}
