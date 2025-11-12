9254558065bbd79f9c1dd1bdac1dc5464386951b
MoniqueMSouza <moniquemoreira.mkt@gmail.com>
2025-11-12 06:56:13 -0300

    feat: programa em C para demonstrar como funciona conversão explícita de dados

---
+++ N3_NC1/NC_NC5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+float numero1 = 10.2;
+int numero2 = 10;
+
+printf("numero1 > numero2: %d\n", numero1 > numero2);
+printf("numero1 ==numero2: %d\n", numero1 ==numero2);
+
+//Fazendo a conversão explícita para (int)
+
+printf("numero1 > numero2: %d\n",(int) numero1> numero2);
+printf("numero1 ==numero2: %d\n",(int) numero1 == numero2);
+
+
+}
